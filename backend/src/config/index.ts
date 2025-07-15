470fefb9274051a03666b60e9bb4293f04103a62
Sachin Gurjar <sachinhlo232@gmail.com>
2025-07-15 11:19:42 +0530

    setup

---
+++ backend/src/config/index.ts
@@ -0,0 +1,41 @@
+import { ENV } from "~/constant";
+import { ENV_TYPE } from "~/types/enum";
+
+const { DEVELOPMENT, TESTING, PRODUCTION } = ENV_TYPE;
+
+const config = {
+  [DEVELOPMENT]: {
+    BASE_URL: "http://localhost:8080",
+    CLIENT_URL: "http://localhost:3000",
+    REDIS: {
+      HOST: "127.0.0.1",
+      PORT: 6379
+    }
+  },
+  [TESTING]: {
+    BASE_URL: "",
+    CLIENT_URL: "",
+    REDIS: {
+      HOST: "127.0.0.1",
+      PORT: 6379
+    }
+  },
+  [PRODUCTION]: {
+    BASE_URL: "",
+    CLIENT_URL: "",
+    REDIS: {
+      HOST: "redis",
+      PORT: 6379
+    }
+
+  },
+};
+
+const envConfig = config[ENV];
+export const BASE_URL = envConfig.BASE_URL;
+export const CLIENT_URL = envConfig.CLIENT_URL;
+export const REDIS_URL = envConfig.REDIS;
+
+export const API_VERSION = "v1";
+
+export const DEFAULT_API_URL = `/api/${API_VERSION}`;
