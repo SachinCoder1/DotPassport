ebe09259bde625ad7b28663eee2af3ecf39fe576
Sachin Gurjar <sachinhlo232@gmail.com>
2025-09-08 20:29:49 +0530

    google analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,6 +56,18 @@ export default function RootLayout({
         <div className="mb-16"></div>
         {children}
       </body>
+
+      {/* Google tag (gtag.js) */}
+      <Script strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-CYLNXVBVP3"></Script>
+      <Script strategy="afterInteractive" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CYLNXVBVP3');
+        `}
+      </Script>
     </html>
   );
 }
